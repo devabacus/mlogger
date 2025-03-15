b452d37232c016eb339ccc02b8907e798a8db9dc
mrfrolk <ekaterita20@gmail.com>
2025-03-15 06:24:56 +0500

    init

---
+++ lib/mlogger.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:talker_flutter/talker_flutter.dart';
+
+abstract class LoggerService {
+  void info(String msg);
+  void warning(String msg);
+  void debug(String msg);
+  void error(dynamic msg, [Object? exception, StackTrace? stackTrace]);
+  void showLogs(BuildContext context);
+}
+
+class TalkerService implements LoggerService {
+  final Talker talker;
+
+  TalkerService(this.talker);
+  
+  // Фабричный конструктор для удобного создания
+  factory TalkerService.init() {
+    return TalkerService(TalkerFlutter.init());
+  }
+
+  @override
+  void debug(String msg) {
+    talker.debug(msg);
+  }
+
+  @override
+  void error(dynamic msg, [Object? exception, StackTrace? stackTrace]) {
+    talker.error(msg, exception, stackTrace);
+  }
+
+  @override
+  void info(String msg) {
+    talker.info(msg);
+  }
+
+  @override
+  void warning(String msg) {
+    talker.warning(msg);
+  }
+  
+  @override
+  void showLogs(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => TalkerScreen(talker: talker)),
+    );
+  }
+}
+
+final log = TalkerService.init();
